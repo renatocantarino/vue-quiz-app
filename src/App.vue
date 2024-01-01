e0ee6a79ba1db60c4ff7963b919391af81b7ce1f
renatocantarino <renato.cantarino@gmail.com>
2024-01-01 15:31:17 -0300

    app evo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ScoreBoard :me="me" :pc="pc" />
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+      <template v-for="(item, idx) in this.answers" :key="idx">
+        <input :disabled="submited" type="radio" name="options" :value="item" v-model="choosen" />
+        <label v-html="item"></label>
+        <br />
+      </template>
+      <button class="send" v-if="!submited" @click="submitChoosen()" type="button">
+        Send
+      </button>
+
+      <section class="result" v-if="submited">
+        <h4 v-if="isCorrect">Resposta Correta</h4>
+        <h4 v-else>
+          Resposta Errada. A resposta correta é
+          <label v-html="this.correctAnswers"></label>
+        </h4>
+        <button class="send" @click="getNew()" type="button">
+          Proxima pergunta
+        </button>
+      </section>
+    </template>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ScoreBoard from '@/components/ScoreBoard.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ScoreBoard,
+  },
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswers: undefined,
+      choosen: undefined,
+      isCorrect: false,
+      submited: false,
+      me: 0,
+      pc: 0,
+    };
+  },
+  methods: {
+    init: function () {
+      this.submited = false;
+      this.choosen = undefined;
+      this.question = undefined;
+      this.isCorrect = false;
+    },
+    getNew: function () {
+      this.init();
+      this.axios
+        .get('https://opentdb.com/api.php?amount=1')
+        .then((response) => {
+          this.question = response.data.results[0].question;
+          this.incorrectAnswers = response.data.results[0].incorrect_answers;
+          this.correctAnswers = response.data.results[0].correct_answer;
+        });
+    },
+    submitChoosen: function () {
+      if (this.choosen) {
+        this.submited = true;
+        this.isCorrect = this.choosen == this.correctAnswers;
+        this.isCorrect ? this.me++ : this.pc++;
+      } else {
+        alert('informe uma resposta');
+      }
+    },
+  },
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(
+        Math.round(Math.random() * answers.length),
+        0,
+        this.correctAnswers
+      );
+      return answers;
+    },
+  },
+  created() {
+    this.getNew();
+  },
+};
 </script>
 
 <style lang="scss">
@@ -21,6 +96,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type='radio'] {
+    margin: 12px 5px;
+  }
+
+  button.send {
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    color: #fff;
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    cursor: pointer;
+    padding: 0 16px;
+  }
 }
 </style>
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="score">
+        Player <span> {{ me }}</span> X <span> {{ pc }}</span> Computer
+    </section>
+</template>
+
+<script>
+export default {
+    props: [ "me" , "pc"]
+};
+</script>
+
+<style lang="scss">
+section.score {
+    border-bottom: 1px solid black;
+    padding: 24px;
+    font-size: 18px;
+
+    span {
+        padding: 18px;
+        font-weight: bold;
+        border: 1px solid black;
+    }
+}
+</style>
